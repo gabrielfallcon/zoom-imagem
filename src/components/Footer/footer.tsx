3ae7c9b4523799ee68fac1d5ea85760008220492
Keverton Reis <kevertondrv@gmail.com>
2026-03-25 23:01:54 -0300

    insert logo, location, social and SEO

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,4 +1,5 @@
-import { FiInstagram, FiMail, FiPhone } from "react-icons/fi";
+import Image from "next/image";
+import { FiFacebook, FiInstagram, FiLinkedin, FiX } from "react-icons/fi";
 import styles from "./footer.module.scss";
 
 const Footer = () => {
@@ -6,41 +7,69 @@ const Footer = () => {
     <div className={styles.footerWrapper}>
       <div className={styles.content}>
         <div className={styles.contentLogo}>
-          {/* <img src="/images/plinapps-white.png" alt="PlinApps" /> */}
+          <Image src="/images/logo-zoom.svg" alt="PlinApps" width={200}
+          height={60}/>
         </div>
 
-        <ul className={styles.listPolicy}>
-          <li>Política de Privacidade</li>
-          <li>Termos de Uso</li>
-          <li>Política de Cookies</li>
+        <ul className={styles.listInstitucional}>
+          <span>Zoom</span>
+          <li>Sobre</li>
+          <li>Notícias</li>
+          <li>Portifólio</li>
+          <li>Linha Eco</li>
         </ul>
 
-        <p className={styles.policy}>
-          Zoom © 2026 Todos os direitos reservados
+        <p className={styles.localization}>
+          <strong>São Paulo</strong><br/>
+          Rua Paulo Emílio Salles Gomes, 45, - Limão
+          São Paulo - SP, 02710-110, Brasil
+        </p>
+
+        <p className={styles.localization}>
+          <strong>Lauro de Freitas</strong><br/>
+          R. Prisco José de Souza, s/n - Lote Amp Recreio Ipitanga, Q 28 - Caji - Lauro de Freitas - BA, 42700-130, Brasil
         </p>
 
         <ul className={styles.listSocialMobile}>
           <li>
             <div>
-              <FiInstagram size={16} color="#000000" />
+              <FiInstagram size={22} color="#000000" />
             </div>
 
-            <span>@zoomimage</span>
+            <span>@zoomimagebr</span>
           </li>
           <li>
             <div>
-              <FiPhone size={16} color="#000000" />
+              <FiX size={22} color="#000000" />
             </div>
 
-            <span>(11) 95798-2407</span>
+            <span>@zoomimagem</span>
           </li>
           <li>
             <div>
-              <FiMail size={16} color="#000000" />
+              <FiFacebook size={22} color="#000000" />
             </div>
 
-            <span>contato@plinapps.com</span>
+            <span>zoomimagembr/</span>
           </li>
+
+          <li>
+            <div>
+              <FiLinkedin size={22} color="#000000" />
+            </div>
+
+            <span>zoom-imagem/</span>
+          </li>
+        </ul>
+
+        
+      </div>
+      <div className={styles.contentSEO}>
+        <ul className={styles.listSEO}>
+          <li>Produção de OOH no Brasil</li>
+          <li>Materiais para PDV</li>
+          <li>Projetos especiais de comunicação visual</li>
+          <li>Execução de campanhas publicitárias</li>
         </ul>
       </div>
     </div>
@@ -48,3 +77,4 @@ const Footer = () => {
 };
 
 export { Footer };
+
