3b454334abc0ae440da0335aa3dc54609b17a1f7
Keverton Reis <kevertondrv@gmail.com>
2026-03-28 21:53:20 -0300

    add zoom head icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/images/plinapps05.svg" />
+        <link rel="icon" href="/images/zoom.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet" />
